ef113242972f700bf0e7154a5c4adae2adb36e50
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-16 21:12:25 +0200

    Add dummy printing example

--- qr-printer/Program.cs
+++ qr-printer/Program.cs
@@ -10,6 +10,23 @@ namespace qr_printer
     {
         static void Main(string[] args)
         {
+            var doc = new bpac.DocumentClass();
+            if (doc.Open("labels/test.lbx"))
+            {
+                doc.GetObject("label").Text = "FooText";
+                doc.GetObject("qr").Text = "FooQr";
+                doc.SetPrinter("Brother QL-810W", false);
+                doc.SetMediaById(doc.Printer.GetMediaId(), true);
+                doc.StartPrint("", bpac.PrintOptionConstants.bpoDefault);
+                doc.PrintOut(1, bpac.PrintOptionConstants.bpoDefault);
+                doc.EndPrint();
+                doc.Close();
+                Console.WriteLine("Label printed");
+            }
+            else
+            {
+                Console.WriteLine("Can't open label file!");
+            }
         }
     }
 }
