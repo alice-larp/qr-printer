2a967373dbe0f78d2528f8d888514fa66f10e658
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-16 21:41:55 +0200

    Add webserver

--- qr-printer/Program.cs
+++ qr-printer/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace qr_printer
+﻿using System;
+using System.Net;
+
+namespace qr_printer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8091/print/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
             new Printer("Brother QL-810W").PrintQr("Hello", "123");
+            return "QR printed!";
         }
     }
 }
