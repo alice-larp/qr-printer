bb3d938b06035f2645c17b7d54b1d2dbc3526299
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-16 21:17:55 +0200

    Extract printing in separate class

---
+++ qr-printer/Printer.cs
@@ -0,0 +1,29 @@
+﻿using bpac;
+using System;
+
+namespace qr_printer
+{
+    class Printer
+    {
+        public static void Print()
+        {
+            var doc = new DocumentClass();
+            if (doc.Open("labels/test.lbx"))
+            {
+                doc.GetObject("label").Text = "FooText";
+                doc.GetObject("qr").Text = "FooQr";
+                doc.SetPrinter("Brother QL-810W", false);
+                doc.SetMediaById(doc.Printer.GetMediaId(), true);
+                doc.StartPrint("", PrintOptionConstants.bpoDefault);
+                doc.PrintOut(1, PrintOptionConstants.bpoDefault);
+                doc.EndPrint();
+                doc.Close();
+                Console.WriteLine("Label printed");
+            }
+            else
+            {
+                Console.WriteLine("Can't open label file!");
+            }
+        }
+    }
+}
--- qr-printer/Program.cs
+++ qr-printer/Program.cs
@@ -1,32 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace qr_printer
+﻿namespace qr_printer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var doc = new bpac.DocumentClass();
-            if (doc.Open("labels/test.lbx"))
-            {
-                doc.GetObject("label").Text = "FooText";
-                doc.GetObject("qr").Text = "FooQr";
-                doc.SetPrinter("Brother QL-810W", false);
-                doc.SetMediaById(doc.Printer.GetMediaId(), true);
-                doc.StartPrint("", bpac.PrintOptionConstants.bpoDefault);
-                doc.PrintOut(1, bpac.PrintOptionConstants.bpoDefault);
-                doc.EndPrint();
-                doc.Close();
-                Console.WriteLine("Label printed");
-            }
-            else
-            {
-                Console.WriteLine("Can't open label file!");
-            }
+            Printer.Print();
         }
     }
 }
